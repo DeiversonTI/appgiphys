9fdd781e690afecb33726550cfc6363df15ec950
felipe barreto <felipebarreto148@gmail.com>
2021-03-25 17:09:37 -0300

    projeto desenvolvido dia 25/03

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <FormSearchGiphy />
+  </div>
+</template>
+
+<script>
+import FormSearchGiphy from "./components/PageSearchGiphy";
+
+export default {
+  name: "App",
+  components: {
+    FormSearchGiphy,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  margin: 0;
+  padding: 0;
+
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Cards/GiphyCards.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="main">
+    <div class="ui link cards">
+      <div class="card">
+        <div class="image">
+          <img :src="urlGiphy" :alt="title" width="150px" height="150px" />
+        </div>
+        <div class="content">
+          <div class="header" v-html="title || 'Gif'"></div>
+          <div class="meta">
+            <a>Friends</a>
+          </div>
+          <!-- <div class="description" v-html="description"></div> -->
+        </div>
+        <div class="extra content">
+          <span class="right floated">
+            import in <span v-html="import_date"></span>
+          </span>
+          <span>
+            <i class="user icon"></i>
+            75 Friends
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GiphyCard",
+  props: ["urlGiphy", "title", "import_date", ],
+};
+</script>
+
+<style scoped>
+img {
+  margin: 25px;
+}
+</style>
